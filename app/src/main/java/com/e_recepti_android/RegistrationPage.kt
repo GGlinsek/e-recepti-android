2ab98d47f5a68bf09fb893cbf654aa3f63aefa74
GGlinsek <97229710+GGlinsek@users.noreply.github.com>
2023-01-04 22:51:42 +0100

    registration page

---
+++ app/src/main/java/com/e_recepti_android/RegistrationPage.kt
@@ -0,0 +1,21 @@
+package com.e_recepti_android
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+class RegistrationPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+
+        val imeUporabnika = findViewById<EditText>(R.id.etIme)
+        val priimekUporabnika = findViewById<EditText>(R.id.etPriimek)
+        val mailUporabnika = findViewById<EditText>(R.id.etMail)
+        val gesloUporabnika = findViewById<EditText>(R.id.etGeslo)
+        val ponoviGeslo = findViewById<EditText>(R.id.etPonoviGeslo)
+        val registrationButton = findViewById<Button>(R.id.btnRegistracija)
+
+    }
+}
