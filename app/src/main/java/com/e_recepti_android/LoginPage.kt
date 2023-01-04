999f218b0636edff9d0a68d95e23e10e34526931
GGlinsek <97229710+GGlinsek@users.noreply.github.com>
2023-01-04 21:34:20 +0100

    login page

---
+++ app/src/main/java/com/e_recepti_android/LoginPage.kt
@@ -0,0 +1,22 @@
+package com.e_recepti_android
+
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val usernameText = findViewById<EditText>(R.id.username)
+        val passwordText = findViewById<EditText>(R.id.password)
+        val registracijaGumb = findViewById<TextView>(R.id.registracija)
+
+        registracijaGumb.setOnClickListener {
+
+        }
+
+    }
+}
--- app/src/main/java/com/e_recepti_android/MainActivity.kt
+++ app/src/main/java/com/e_recepti_android/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.e_recepti_android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val loginButton = findViewById<Button>(R.id.profile)
+        loginButton.setOnClickListener {
+            val intent = Intent(this, LoginPage::class.java)
+            startActivity(intent)
+        }
     }
 }
